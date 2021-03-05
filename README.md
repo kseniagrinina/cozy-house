5699ca3b46f213b54a5dcae1636d675b2dfc6b9d
Ksenia Grinina <ksen.grinina@gmail.com>
2021-03-05 12:45:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cozy House
+
+https://cozy-house.netlify.app/
+
+Test task for P2H
+The webpage corresponds to the mockup at screen width of 1920px;
+Desktop first responsive design;
+Preprocessor - SASS;
+Bundler - Parcel;
+Vanilla JS
+
+Made with love 💜
---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+// import Swiper from "swiper/bundle";
+import Swiper, { Navigation } from "swiper";
+Swiper.use([Navigation]);
+import "swiper/swiper-bundle.css";
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".header__burger-menu");
+  const menu = document.querySelector(".header__menu");
+
+  burger.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+
+  menu.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+
+  const swiper = new Swiper(".swiper-container", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    breakpoints: {
+      991: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+  };
+});
